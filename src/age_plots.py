947b5270b7814d4097e625e545d3a1296dc2227c
mutnehme <63080364+mutnehme@users.noreply.github.com>
2021-11-26 21:41:47 -0800

    Update age_plots.py
    Added function for sex demographics and plotting as well

--- src/age_plots.py
+++ src/age_plots.py
@@ -13,6 +13,7 @@ Basic data processing and importing. A pyplot call is used, and one image is cre
 from src.MigrationData import MigrationData
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def correct_year(year):
     '''
@@ -59,6 +60,34 @@ def get_age_group_data_allyrs(years, ispercent):
         age_group_data[year] = l_year_info[year-2010].get_age_group_data(ispercent)
     return age_group_data
 
+def get_sex_group_data_allyrs(years, ispercent):
+    '''
+    Function to get age information for all years and return it as a dict of pandas.core.frame.DataFrame
+    
+    Parameters
+    ----------
+    ispercent: bool
+        whether the return type should be in percent or not
+    years: tuple
+        contains a tuple of ints (years)
+        
+    Returns
+    -------
+    age_group_data: dict of pandas.core.frame.DataFrame 
+        dataframes for the given years
+    
+    '''
+    assert isinstance(ispercent, bool)
+    assert isinstance(years, tuple)
+    l_year_info = [MigrationData(2010)]*len(years)
+    age_group_data = {}
+    for year in years:
+        correct_year(year)
+        l_year_info[year-2010] = MigrationData(year)
+        l_year_info[year-2010].load_dframe()
+        age_group_data[year] = l_year_info[year-2010].get_sex_group_data(ispercent).astype('int32')
+    return age_group_data
+
 def get_age_info_years_state(data, state, years):
     '''
     Function to get all years info for a certain state. This is assumed to come after the 
@@ -83,7 +112,7 @@ def get_age_info_years_state(data, state, years):
     assert isinstance(data,dict)
     for year in years: #check the years are correct!
         correct_year(year)
-        assert (state in a[year].axes[0])
+        assert (state in data[year].axes[0])
     cols = ('1 to 17 years', '18 to 24 years', '25 to 54 years', '55 years and over')
     all_years_population_data_state = []
     for year in years:
@@ -93,10 +122,26 @@ def get_age_info_years_state(data, state, years):
     
 if  __name__ == '__main__':
     years = (2010,2011,2012,2013,2014,2015,2016,2017,2018, 2019)
-    keys = ('1 to 17 years', '18 to 24 years', '25 to 54 years', '55 years and over')
-    a = get_age_group_data_allyrs(years,False);
+    #keys = ('1 to 17 years', '18 to 24 years', '25 to 54 years', '55 years and over')
     
-    california_info_by_age = get_age_info_years_state(a, 'California', years)
-    california_info_by_age.plot(title="California's inflow migration by age group")
-    plt.show()
-    plt.close("all")
+    #Plotting age info for California
+    age_data_allyrs = get_age_group_data_allyrs(years,True);
+    #Graphing of California_migration_pyplot.png
+    california_info_by_age = get_age_info_years_state(age_data_allyrs, 'California', years)
+    plot_cali = california_info_by_age.plot(title="California's inflow migration by age group")
+    
+    #Plotting sex data, specifically the difference in the male and female migrants
+    sex_data = get_sex_group_data_allyrs(years,False)
+    for year in years:
+        sex_data[year] = sex_data[year].sum()
+    sex_data_df = pd.DataFrame(sex_data.values(), index = years, columns = ['Male', 'Female'])
+    #initial_sex_dif = sex_data_df['Male'][2010] - sex_data_df['Female'][2010]
+    #fin_sex_dif = sex_data_df['Male'][2019] - sex_data_df['Female'][2019]
+    sex_data_df['difference'] = sex_data_df['Male'] - sex_data_df['Female']
+    ax = sex_data_df['difference'].plot.bar(rot = 0, title = "Closing gap between Male and Female migrations from 2010 to 2019 in the US.")
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.get_legend().remove()
+    ax.set_ylabel('Total male migrants - total female migrants')
+    ax.ticklabel_format(useOffset=False, style='plain', axis='y')
+    ax.plot()
